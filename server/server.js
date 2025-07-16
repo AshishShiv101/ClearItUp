76941e738d23babbaa5ee5e9c3163d367829ce17
Ashish Shiv <110735303+AshishShiv101@users.noreply.github.com>
2025-07-16 13:23:16 +0530

    routes created

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 const PORT = process.env.PORT || 4000
 
@@ -12,5 +13,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/',(req,res)=> res.send("API WORKING"))
+app.use('/api/user',userRouter)
+
 
 app.listen(PORT,()=> console.log("HEHEHHE"))
